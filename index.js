90afb60b88934f59481062a36d290713ea11390c
react7967 <react7967@caelumBSB01-22>
2018-09-18 22:57:07 -0300

    Dia 2

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import Feed from './src/components/Feed';
 
-AppRegistry.registerComponent(appName, () => App);
+//import App from './App';
+//import {name as appName} from './app.json';
+
+AppRegistry.registerComponent('InstaluraMobile', () => Feed);
---
+++ src/components/Feed.js
@@ -0,0 +1,41 @@
+
+import React, { Component } from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+import Post from './Post';
+
+export default class Feed extends Component {
+
+    constructor() {
+
+        super();
+        this.state = {
+            fotos: []
+        }
+    }
+
+    componentDidMount() {
+
+        fetch('https://instalura-api.herokuapp.com/api/public/fotos/rafael')
+            .then(resposta => resposta.json())
+            .then(json => this.setState({fotos: json}));
+    }
+
+    render() {
+
+        return (
+            <FlatList style={styles.container}
+                keyExtractor={item => String(item.id)}
+                data={this.state.fotos}
+                renderItem={ ({item}) =>
+                    <Post foto={item} />
+                }
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 20,
+    },
+});
